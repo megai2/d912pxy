f1f3b074cce9bce25a59ff57ca4bc42685738299
megai2 <megai2@ya.ru>
2018-12-19 14:28:25 +0300

    update v0.9.1a
    -rearranged dirs
    -added bat files to easy install/remove
    -lowered requirements for DXGI and DX12 interface versions

---
+++ install_ps.bat
@@ -0,0 +1,4 @@
+rename ..\bin64\d3d9.dll d3d9_backup.dll
+copy bin64\d3d9ps.dll ..\bin64\d3d9.dll
+copy bin64\P7x64.dll ..\bin64\P7x64.dll
+pause
---
+++ remove.bat
@@ -0,0 +1,4 @@
+del ..\bin64\d3d9.dll
+del ..\bin64\P7x64.dll
+rename ..\bin64\d3d9_backup.dll d3d9.dll
+pause
