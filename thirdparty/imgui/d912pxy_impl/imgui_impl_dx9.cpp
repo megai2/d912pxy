92fdb2099170da7de3ccf6831cf944550370220c
megai2 <megai2@ya.ru>
2019-01-31 15:37:26 +0300

    patch for imgui dx9 implementation
    -changed fixed pipeline drawing to programmable pipeline drawing
    -removed stateblocks
    -changed PSO related api calls to d912pxy PSO dx9 api hack
    -removed vertex repacking

--- thirdparty/imgui/d912pxy_impl/imgui_impl_dx9.cpp
+++ thirdparty/imgui/d912pxy_impl/imgui_impl_dx9.cpp
@@ -32,6 +32,244 @@ struct CUSTOMVERTEX
 };
 #define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZ|D3DFVF_DIFFUSE|D3DFVF_TEX1)
 
+//d912pxy ==============
+
+static float g_oldDisplaySize[2] = { 0,0 };
+static int g_d912pxy_present = 0;
+static IDirect3DVertexShader9* g_pVS = NULL;
+static IDirect3DPixelShader9* g_pPS = NULL;
+static IDirect3DVertexDeclaration9* g_pVDecl = NULL;
+static void* g_pPSO = NULL;
+static IDirect3DVertexBuffer9*  g_pVB2 = NULL;
+#define D3DRS_ENABLE_D912PXY_API_HACKS (D3DRENDERSTATETYPE)220
+#define D3DRS_D912PXY_ENQUEUE_PSO_COMPILE (D3DRENDERSTATETYPE)221
+#define D3DRS_D912PXY_SETUP_PSO (D3DRENDERSTATETYPE)222
+
+#define D3DDECLMETHOD_PER_VERTEX_CONSTANT 8
+
+DWORD g_pVS_function[] = {
+	0xFFFE0300, 0x0014FFFE, 0x42415443, 0x0000001C, 0x00000023, 0xFFFE0300, 0x00000000, 0x00000000, 0x00000100, 0x0000001C,
+	0x335F7376, 0x4D00305F, 0x6F726369, 0x74666F73, 0x29522820, 0x534C4820, 0x6853204C, 0x72656461, 0x6D6F4320, 0x656C6970,
+	0x30312072, 0xAB00312E, 0x05000051, 0xA00F0000, 0xBF000000, 0x3F000000, 0x3F800000, 0x00000000, 0x0200001F, 0x80000000,
+	0x900F0000, 0x0200001F, 0x80000005, 0x900F0001, 0x0200001F, 0x8000000A, 0x900F0002, 0x0200001F, 0x80010005, 0x900F0003,
+	0x0200001F, 0x80000000, 0xE00F0000, 0x0200001F, 0x8000000A, 0xE00F0001, 0x0200001F, 0x80000005, 0xE0030002, 0x04000004,
+	0x800F0000, 0x90040000, 0xA0FA0000, 0xA0BF0000, 0x02000001, 0x80060001, 0xA0FF0000, 0x03000002, 0x800F0002, 0xA0140000,
+	0x90500003, 0x02000006, 0x80010002, 0x80000002, 0x03000002, 0x80010001, 0x80000002, 0x80000002, 0x03000002, 0x80030003,
+	0xA0550000, 0x91E10003, 0x02000006, 0x80010002, 0x80550003, 0x02000006, 0x80010003, 0x80000003, 0x03000002, 0x80020003,
+	0x80000003, 0x80000003, 0x03000005, 0x80080001, 0x80000002, 0x80550002, 0x03000009, 0xE0010000, 0x80E40000, 0x80E40001,
+	0x02000006, 0x80010000, 0x80FF0002, 0x03000005, 0x80080000, 0x80000000, 0x80AA0002, 0x03000005, 0x80070000, 0xA0FA0000,
+	0x90C40000, 0x02000001, 0x800D0003, 0xA0B70000, 0x03000009, 0xE0020000, 0x80E40000, 0x80E40003, 0x02000001, 0xE00C0000,
+	0xA0940000, 0x02000001, 0xE00F0001, 0x90E40002, 0x02000001, 0xE0030002, 0x90E40001, 0x0000FFFF
+};
+
+DWORD g_pPS_function[] = {
+	0xFFFF0300, 0x001FFFFE, 0x42415443, 0x0000001C, 0x0000004F, 0xFFFF0300, 0x00000001, 0x0000001C, 0x00000100, 0x00000048,
+	0x00000030, 0x00000003, 0x00000001, 0x00000038, 0x00000000, 0x44325F73, 0xABABAB00, 0x000C0004, 0x00010001, 0x00000001,
+	0x00000000, 0x335F7370, 0x4D00305F, 0x6F726369, 0x74666F73, 0x29522820, 0x534C4820, 0x6853204C, 0x72656461, 0x6D6F4320,
+	0x656C6970, 0x30312072, 0xAB00312E, 0x0200001F, 0x8000000A, 0x900F0000, 0x0200001F, 0x80000005, 0x90030001, 0x0200001F,
+	0x90000000, 0xA00F0800, 0x03000042, 0x800F0000, 0x90E40001, 0xA0E40800, 0x03000005, 0x800F0800, 0x80E40000, 0x90E40000,
+	0x0000FFFF
+};
+
+void ImGui_ImplDX9_RenderDrawLists_d912pxy(ImDrawData* draw_data)
+{
+	// Avoid rendering when minimized
+	ImGuiIO& io = ImGui::GetIO();
+	if (io.DisplaySize.x <= 0.0f || io.DisplaySize.y <= 0.0f)
+		return;
+
+	//megai2: if PSO is not compiled yet, ignore all draws
+	if (!g_pPSO)
+		return;
+
+	// Create and grow buffers if needed
+	if (!g_pVB || g_VertexBufferSize < draw_data->TotalVtxCount)
+	{
+		if (g_pVB) { g_pVB->Release(); g_pVB = NULL; }
+		g_VertexBufferSize = draw_data->TotalVtxCount + 5000;
+		if (g_pd3dDevice->CreateVertexBuffer(g_VertexBufferSize * sizeof(ImDrawVert), D3DUSAGE_DYNAMIC | D3DUSAGE_WRITEONLY, 0, D3DPOOL_DEFAULT, &g_pVB, NULL) < 0)
+			return;
+	}
+	if (!g_pIB || g_IndexBufferSize < draw_data->TotalIdxCount)
+	{
+		if (g_pIB) { g_pIB->Release(); g_pIB = NULL; }
+		g_IndexBufferSize = draw_data->TotalIdxCount + 10000;
+		if (g_pd3dDevice->CreateIndexBuffer(g_IndexBufferSize * sizeof(ImDrawIdx), D3DUSAGE_DYNAMIC | D3DUSAGE_WRITEONLY, sizeof(ImDrawIdx) == 2 ? D3DFMT_INDEX16 : D3DFMT_INDEX32, D3DPOOL_DEFAULT, &g_pIB, NULL) < 0)
+			return;
+	}
+
+	// Copy and convert all vertices into a single contiguous buffer
+	ImDrawVert* vtx_dst;
+	ImDrawIdx* idx_dst;
+	if (g_pVB->Lock(0, (UINT)(draw_data->TotalVtxCount * sizeof(ImDrawVert)), (void**)&vtx_dst, D3DLOCK_DISCARD) < 0)
+		return;
+	if (g_pIB->Lock(0, (UINT)(draw_data->TotalIdxCount * sizeof(ImDrawIdx)), (void**)&idx_dst, D3DLOCK_DISCARD) < 0)
+		return;
+	for (int n = 0; n < draw_data->CmdListsCount; n++)
+	{
+		const ImDrawList* cmd_list = draw_data->CmdLists[n];
+		
+		memcpy(vtx_dst, cmd_list->VtxBuffer.Data, cmd_list->VtxBuffer.Size * sizeof(ImDrawVert));
+		memcpy(idx_dst, cmd_list->IdxBuffer.Data, cmd_list->IdxBuffer.Size * sizeof(ImDrawIdx));
+
+		vtx_dst += cmd_list->VtxBuffer.Size;
+		idx_dst += cmd_list->IdxBuffer.Size;
+	}
+	g_pVB->Unlock();
+	g_pIB->Unlock();
+	g_pd3dDevice->SetStreamSource(0, g_pVB, 0, sizeof(ImDrawVert));
+	g_pd3dDevice->SetStreamSource(1, g_pVB2, 0, 8);
+	g_pd3dDevice->SetIndices(g_pIB);
+	
+	// Setup viewport
+	D3DVIEWPORT9 vp;
+	vp.X = vp.Y = 0;
+	vp.Width = (DWORD)io.DisplaySize.x;
+	vp.Height = (DWORD)io.DisplaySize.y;
+	vp.MinZ = 0.0f;
+	vp.MaxZ = 1.0f;
+	g_pd3dDevice->SetViewport(&vp);
+
+	// Setup render state: fixed-pipeline, alpha-blending, no face culling, no depth testing
+	g_pd3dDevice->SetSamplerState(0, D3DSAMP_MINFILTER, D3DTEXF_LINEAR);
+	g_pd3dDevice->SetSamplerState(0, D3DSAMP_MAGFILTER, D3DTEXF_LINEAR);
+
+	g_pd3dDevice->SetRenderState(D3DRS_ALPHATESTENABLE, false);
+	g_pd3dDevice->SetRenderState(D3DRS_SCISSORTESTENABLE, true);
+
+	// Setup orthographic projection matrix
+	// Being agnostic of whether <d3dx9.h> or <DirectXMath.h> can be used, we aren't relying on D3DXMatrixIdentity()/D3DXMatrixOrthoOffCenterLH() or DirectX::XMMatrixIdentity()/DirectX::XMMatrixOrthographicOffCenterLH()
+	{
+		float R = io.DisplaySize.x;
+		float B = io.DisplaySize.y;
+
+		if ((g_oldDisplaySize[0] != R) || (g_oldDisplaySize[1] != B))
+		{
+			g_oldDisplaySize[0] = R + 0.5f;
+			g_oldDisplaySize[1] = B + 0.5f;
+
+			float* viewRect;
+
+			if (g_pVB2->Lock(0, 0, (void**)&viewRect, 0) < 0)
+				return;
+
+			memcpy(viewRect, g_oldDisplaySize, 8);
+
+			g_pVB2->Unlock();
+		}
+	}
+
+	// Render command lists
+	int vtx_offset = 0;
+	int idx_offset = 0;
+	for (int n = 0; n < draw_data->CmdListsCount; n++)
+	{
+		const ImDrawList* cmd_list = draw_data->CmdLists[n];
+		for (int cmd_i = 0; cmd_i < cmd_list->CmdBuffer.Size; cmd_i++)
+		{
+			const ImDrawCmd* pcmd = &cmd_list->CmdBuffer[cmd_i];
+			if (pcmd->UserCallback)
+			{
+				pcmd->UserCallback(cmd_list, pcmd);
+			}
+			else
+			{
+				const RECT r = { (LONG)pcmd->ClipRect.x, (LONG)pcmd->ClipRect.y, (LONG)pcmd->ClipRect.z, (LONG)pcmd->ClipRect.w };
+				g_pd3dDevice->SetTexture(0, (LPDIRECT3DTEXTURE9)pcmd->TextureId);
+				g_pd3dDevice->SetScissorRect(&r);
+
+				g_pd3dDevice->GetRenderState(D3DRS_D912PXY_SETUP_PSO, (DWORD*)g_pPSO);
+
+				g_pd3dDevice->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, vtx_offset, 0, (UINT)cmd_list->VtxBuffer.Size, idx_offset, pcmd->ElemCount / 3);
+			}
+			idx_offset += pcmd->ElemCount;
+		}
+		vtx_offset += cmd_list->VtxBuffer.Size;
+	}
+
+	g_pd3dDevice->SetRenderState(D3DRS_SCISSORTESTENABLE, false);
+	g_pd3dDevice->SetStreamSource(1, NULL, 0, 0);
+}
+
+static bool ImGui_ImplDX9_Release_d912pxy_objects()
+{
+	if (g_pVB2)
+	{
+		g_pVB2->Release();
+		g_pVB2 = NULL;
+	}
+
+	if (g_pVS)
+	{
+		g_pVS->Release();
+		g_pVS = NULL;
+	}
+
+	if (g_pPS)
+	{
+		g_pPS->Release();
+		g_pPS = NULL;
+	}
+
+	if (g_pVDecl)
+	{
+		g_pVDecl->Release();
+		g_pVDecl = NULL;
+	}
+
+	g_pPSO = NULL;
+
+	return true;
+}
+
+static bool ImGui_ImplDX9_Create_d912pxy_objects()
+{
+	if (g_pd3dDevice->CreateVertexShader(g_pVS_function, &g_pVS) < 0)
+		return false;
+
+	if (g_pd3dDevice->CreatePixelShader(g_pPS_function, &g_pPS) < 0)
+		return false;
+
+	/*WORD    Stream;     // Stream index
+	WORD    Offset;     // Offset in the stream in bytes
+	BYTE    Type;       // Data type
+	BYTE    Method;     // Processing method
+	BYTE    Usage;      // Semantics
+	BYTE    UsageIndex; // Semantic index*/
+
+	D3DVERTEXELEMENT9 vDclElements[] = {
+		{0,0,D3DDECLTYPE_FLOAT2,0,D3DDECLUSAGE_POSITION,0},
+		{0,8,D3DDECLTYPE_FLOAT2,0,D3DDECLUSAGE_TEXCOORD,0},
+		{0,16,D3DDECLTYPE_D3DCOLOR,0,D3DDECLUSAGE_COLOR,0},
+		{1,0,D3DDECLTYPE_FLOAT2,D3DDECLMETHOD_PER_VERTEX_CONSTANT,D3DDECLUSAGE_TEXCOORD,1},
+		{0xFF,0,D3DDECLTYPE_UNUSED,0,0,0}
+	};
+
+	if (g_pd3dDevice->CreateVertexDeclaration(&vDclElements[0],&g_pVDecl) < 0)
+		return false;
+
+	if (g_pd3dDevice->CreateVertexBuffer(16, 0, 0, D3DPOOL_DEFAULT, &g_pVB2, 0) < 0)
+		return false;
+
+	g_pd3dDevice->SetPixelShader(g_pPS);
+	g_pd3dDevice->SetVertexShader(g_pVS);
+	g_pd3dDevice->SetVertexDeclaration(g_pVDecl);
+	g_pd3dDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
+	g_pd3dDevice->SetRenderState(D3DRS_LIGHTING, false);
+	g_pd3dDevice->SetRenderState(D3DRS_ZENABLE, false);
+	g_pd3dDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, true);	
+	g_pd3dDevice->SetRenderState(D3DRS_BLENDOP, D3DBLENDOP_ADD);
+	g_pd3dDevice->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA);
+	g_pd3dDevice->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA);	
+	
+	if (g_pd3dDevice->GetRenderState(D3DRS_D912PXY_ENQUEUE_PSO_COMPILE, (DWORD*)&g_pPSO) < 0)
+		return false;
+
+	return true;
+}
+
+//d912pxy ==============
+
 // This is the main rendering function that you have to implement and provide to ImGui (via setting up 'RenderDrawListsFn' in the ImGuiIO structure)
 // If text or lines are blurry when integrating ImGui in your engine:
 // - in your Render function, try translating your projection matrix by (0.5f,0.5f) or (0.375f,0.375f)
@@ -249,7 +487,13 @@ bool    ImGui_ImplDX9_Init(void* hwnd, IDirect3DDevice9* device)
     io.KeyMap[ImGuiKey_Y] = 'Y';
     io.KeyMap[ImGuiKey_Z] = 'Z';
 
-    io.RenderDrawListsFn = ImGui_ImplDX9_RenderDrawLists;   // Alternatively you can set this to NULL and call ImGui::GetDrawData() after ImGui::Render() to get the same ImDrawData pointer.
+	g_d912pxy_present = device->SetRenderState(D3DRS_ENABLE_D912PXY_API_HACKS, 1) == 343434;
+
+	if (g_d912pxy_present)
+	    io.RenderDrawListsFn = ImGui_ImplDX9_RenderDrawLists_d912pxy;   // Alternatively you can set this to NULL and call ImGui::GetDrawData() after ImGui::Render() to get the same ImDrawData pointer.
+	else 
+		io.RenderDrawListsFn = ImGui_ImplDX9_RenderDrawLists;   // Alternatively you can set this to NULL and call ImGui::GetDrawData() after ImGui::Render() to get the same ImDrawData pointer.
+
     io.ImeWindowHandle = g_hWnd;
 
     return true;
@@ -294,6 +538,10 @@ bool ImGui_ImplDX9_CreateDeviceObjects()
         return false;
     if (!ImGui_ImplDX9_CreateFontsTexture())
         return false;
+
+	if (g_d912pxy_present)
+		return ImGui_ImplDX9_Create_d912pxy_objects();			
+
     return true;
 }
 
@@ -316,6 +564,10 @@ void ImGui_ImplDX9_InvalidateDeviceObjects()
         tex->Release();
         ImGui::GetIO().Fonts->TexID = 0;
     }
+
+	if (g_d912pxy_present)
+		ImGui_ImplDX9_Release_d912pxy_objects();
+
     g_FontTexture = NULL;
 }
 
