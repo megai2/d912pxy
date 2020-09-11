e6b8abe29f92644bec0034b1e667b8f7a8a6f7ba
Dmitry Tsarevich <dimhotepus@gmail.com>
2020-09-12 02:01:14 +0300

    [perf] d912pxy: Delete nullptr is noop. Remove redudant check

--- d912pxy/d912pxy_shader_db.cpp
+++ d912pxy/d912pxy_shader_db.cpp
@@ -44,8 +44,7 @@ void d912pxy_shader_db::UnInit()
 	for (auto iter = shaderPairs.begin(); iter < shaderPairs.end(); ++iter)
 	{
 		d912pxy_shader_pair*& pair = iter.value();
-		if (pair)
-			delete pair;
+		delete pair;
 	}
 
 	d912pxy_noncom::UnInit();
@@ -121,7 +120,6 @@ void d912pxy_shader_db::DeletePair(d912pxy_shader_pair_hash_type ha)
 			pair = *ref.val;
 	}
 
-	if (pair)
-		delete pair;
+	delete pair;
 
 }
