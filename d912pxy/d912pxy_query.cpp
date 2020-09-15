43a8f3ee47e27a8617c69291aba1ff1709a5639c
megai2 <megai2@ya.ru>
2020-09-16 00:19:22 +0300

    use fake results for timestamp related queries
    will make code that is depends on them work, but with CPU timestamps

--- d912pxy/d912pxy_query.cpp
+++ d912pxy/d912pxy_query.cpp
@@ -68,8 +68,23 @@ D912PXY_METHOD_IMPL_NC(GetData)(THIS_ void* pData, DWORD dwSize, DWORD dwGetData
 {
 	LOG_DBG_DTDM(__FUNCTION__);
 
-	if (dwSize == 4)
-		((DWORD*)pData)[0] = 1;
+	switch (m_type)
+	{
+	case D3DQUERYTYPE_TIMESTAMPDISJOINT:
+		if (dwSize == 4)
+			((DWORD*)pData)[0] = 0;
+		break;
+	case D3DQUERYTYPE_TIMESTAMPFREQ:
+		if (dwSize == 8)
+			d912pxy_s.dx12.que.GetDXQue()->GetTimestampFrequency((UINT64*)pData);
+		break;
+	case D3DQUERYTYPE_TIMESTAMP:
+		QueryPerformanceCounter((LARGE_INTEGER*)pData);
+		break;
+	default:
+		if (dwSize == 4)
+			((DWORD*)pData)[0] = 1;
+	}
 
 	return S_OK;
 }
